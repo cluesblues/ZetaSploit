c53e6915c433f43ace77b50fbbd668291f957cf5
Ivan Nikolsky <enty8080@gmail.com>
2021-01-31 13:11:49 +0300

    Update modules_db.py

--- core/commands/main/modules_db.py
+++ core/commands/main/modules_db.py
@@ -42,7 +42,7 @@ class ZetaSploitCommand:
             'Category': "database",
             'Name': "modules_db",
             'Description': "Manage modules databases.",
-            'Usage': "modules_db [-l|-r <name>|-a <name> <path>]",
+            'Usage': "modules_db [-l|-d <name>|-c <name> <path>]",
             'ArgsCount': 1,
             'NeedsArgs': True,
             'Args': list()
@@ -51,22 +51,25 @@ class ZetaSploitCommand:
     def run(self):
         choice = self.details['Args'][0]
         if choice == "-l":
-            databases_data = list()
-            number = 0
-            headers = ("Number", "Name", "Path")
-            databases = self.storage.get("connected_modules_databases")
-            for name in databases.keys():
-                databases_data.append((number, name, databases[name]['path']))
-                number += 1
-            self.io.output("")
-            self.formatter.format_table("Connected Modules Databases", headers, *databases_data)
-            self.io.output("")
-        elif choice == '-r':
+            if self.storage.get("connected_modules_databases"):
+                databases_data = list()
+                number = 0
+                headers = ("Number", "Name", "Path")
+                databases = self.storage.get("connected_modules_databases")
+                for name in databases.keys():
+                    databases_data.append((number, name, databases[name]['path']))
+                    number += 1
+                self.io.output("")
+                self.formatter.format_table("Connected Modules Databases", headers, *databases_data)
+                self.io.output("")
+            else:
+                self.badges.output_warning("No modules database connected.")
+        elif choice == '-d':
             if len(self.details['Args']) < 2:
                 self.badges.output_usage(self.details['Usage'])
             else:
                 self.db.disconnect_modules_database(self.details['Args'][1])
-        elif choice == '-a':
+        elif choice == '-c':
             if len(self.details['Args']) < 3:
                 self.badges.output_usage(self.details['Usage'])
             else:
