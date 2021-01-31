65e3553fa4d773dfbdc8746cea7bfd55df402aa7
Ivan Nikolsky <enty8080@gmail.com>
2021-01-31 12:44:37 +0300

    Update modules_db.py

--- core/commands/main/modules_db.py
+++ core/commands/main/modules_db.py
@@ -65,17 +65,11 @@ class ZetaSploitCommand:
             if len(self.details['Args']) < 2:
                 self.badges.output_usage(self.details['Usage'])
             else:
-                if self.details['Args'][1] in self.storage.get("connected_modules_databases"):
-                    self.db.disconnect_modules_database(self.details['Args'][1])
-                else:
-                    self.badges.output_error("No such connected modules database!")
+                self.db.disconnect_modules_database(self.details['Args'][1])
         elif choice == '-a':
             if len(self.details['Args']) < 3:
                 self.badges.output_usage(self.details['Usage'])
             else:
-                if self.details['Args'][1] not in self.storage.get("connected_modules_databases"):
-                    self.db.connect_modules_database(self.details['Args'][1], self.details['Args'][2])
-                else:
-                    self.badges.output_error("Failed to connect database.")
+                self.db.connect_modules_database(self.details['Args'][1], self.details['Args'][2])
         else:
             self.badges.output_usage(self.details['Usage'])
