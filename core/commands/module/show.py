8edca194ad5893645b5acf5acba2c561cfc89eec
Ivan Nikolsky <enty8080@gmail.com>
2021-01-31 15:33:26 +0300

    Update show.py

--- core/commands/module/show.py
+++ core/commands/module/show.py
@@ -52,20 +52,17 @@ class ZetaSploitCommand:
 
     def show_plugins(self):
         plugins = self.storage.get("plugins")
-        if plugins:
-            plugins_data = list()
-            number = 0
-            headers = ("Number", "Name", "Database", "Description")
-            for database in plugins.keys():
-                plugins = plugins[database]
-                for plugin in sorted(plugins.keys()):
-                    plugins_data.append((number, plugin, database, plugins[plugin]['Description']))
-                    number += 1
-            self.io.output("")
-            self.formatter.format_table("Plugins", headers, *plugins_data)
-            self.io.output("")
-        else:
-            self.badges.output_warning("No plugins available!")
+        plugins_data = list()
+        number = 0
+        headers = ("Number", "Name", "Database", "Description")
+        for database in plugins.keys():
+            plugins = plugins[database]
+            for plugin in sorted(plugins.keys()):
+                plugins_data.append((number, plugin, database, plugins[plugin]['Description']))
+                number += 1
+        self.io.output("")
+        self.formatter.format_table("Plugins", headers, *plugins_data)
+        self.io.output("")
         
     def show_modules(self, information):
         modules = self.storage.get("modules")
@@ -82,47 +79,64 @@ class ZetaSploitCommand:
         self.io.output("")
         self.formatter.format_table(information.title() + " Modules", headers, *modules_data)
         self.io.output("")
-            
+        
     def show_options(self):
         current_module = self.storage.get_array("current_module", self.storage.get("pwd"))
-        if hasattr(current_module, "options"):
-            options_data = list()
-            headers = ("Option", "Value", "Required", "Description")
-            options = current_module.options
-            for option in sorted(options.keys()):
-                value, required = options[option]['Value'], options[option]['Required']
-                if required:
-                    required = "yes"
-                else:
-                    required = "no"
-                if not value and value != 0:
-                    value = ""
-                options_data.append((option, value, required, options[option]['Description']))
-            self.io.output("")
-            self.formatter.format_table("Module Options", headers, *options_data)
-            self.io.output("")
+        options_data = list()
+        headers = ("Option", "Value", "Required", "Description")
+        options = current_module.options
+        for option in sorted(options.keys()):
+            value, required = options[option]['Value'], options[option]['Required']
+            if required:
+                required = "yes"
+            else:
+                required = "no"
+            if not value and value != 0:
+                value = ""
+            options_data.append((option, value, required, options[option]['Description']))
+        self.io.output("")
+        self.formatter.format_table("Module Options", headers, *options_data)
+        self.io.output("")
+        
+    def print_usage(self, informations, plugins, options):
+        usage = "Informations: "
+        for information in informations:
+            usage += information + ", "
+        if plugins:
+            usage += "plugins"
         else:
-            self.badges.output_warning("Module does not have options.")
+            if options:
+                usage += "options"
+            else:
+                usage = usage[:-1]
+        self.badges.output_information(usage)
         
     def run(self):
         information = self.details['Args'][0]
+        current_module = self.storage.get_array("current_module", self.storage.get("pwd"))
+        
+        options = False
+        if hasattr(current_module, "options"):
+            options = True
+        
         modules = self.storage.get("modules")
+        plugins = self.storage.get("plugins")
         
         informations = list()
-        for database in sorted(modules.keys()):
-            for category in sorted(modules[database].keys()):
-                informations.append(category)
+        if modules:
+            for database in sorted(modules.keys()):
+                for category in sorted(modules[database].keys()):
+                    informations.append(category)
         
-        if information == "plugins":
-            self.show_plugins()
-        elif information == "options":
-            self.show_options()
+        if plugins:
+            if information == "plugins":
+                self.show_plugins()
+                return
+        if options:
+            if information == "options":
+                self.show_options()
+                return
+        if information in informations:
+            self.show_modules(information)
         else:
-            if information in informations:
-                self.show_modules(information)
-            else:
-                usage = "Informations: "
-                for information in informations:
-                    usage += information + ", "
-                usage += "plugins"
-                self.badges.output_information(usage)
+            self.print_usage(informations, plugins, options)
