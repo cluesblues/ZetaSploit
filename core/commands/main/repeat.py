758c791e0b277b76abdfce7e635b56961452f4ad
Ivan Nikolsky <enty8080@gmail.com>
2021-02-01 19:06:01 +0300

    Update repeat.py

--- core/commands/main/repeat.py
+++ core/commands/main/repeat.py
@@ -53,7 +53,7 @@ class ZetaSploitCommand:
                 arguments = command.replace(commands[0], "", 1).strip()
         
             if commands:
-                for time in range(1, int(times)):
+                for time in range(int(times)):
                     if not self.execute.execute_core_command(commands, arguments, "main"):
                         if not self.execute.execute_plugin_command(commands, arguments):
                             self.badges.output_error("Unrecognized command!")
