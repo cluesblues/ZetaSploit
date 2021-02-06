2aa0c8fbf2469c7bd1c46a49e2ee23e330b631dc
Ivan Nikolsky <enty8080@gmail.com>
2021-02-06 14:02:44 +0300

    Update jailbroken_or_not.py

--- modules/auxiliary/iphoneos/checker/jailbroken_or_not.py
+++ modules/auxiliary/iphoneos/checker/jailbroken_or_not.py
@@ -45,7 +45,7 @@ class ZetaSploitModule:
             'Description': "Check if remote iPhone jailbroken.",
             'Dependencies': [
                 ''
-            ]
+            ],
             'Comments': [
                 'Remote iPhone jailbroken if 22 port opened on it.',
                 'Cydia.app opens this port by default for SSH connections.'
