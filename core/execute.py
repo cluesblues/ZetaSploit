a1cafc55fc4d1de7a2c5b44b5d5b134647e749dc
Ivan Nikolsky <enty8080@gmail.com>
2021-02-06 17:38:10 +0300

    Update execute.py

--- core/execute.py
+++ core/execute.py
@@ -58,7 +58,7 @@ class execute:
                     self.badges.output_usage(command.details['Usage'])
                 else:
                     args = self.formatter.format_arguments(arguments)
-                    if args:
+                    if args[0]:
                         command.details['Args'] = args
                     try:
                         command.run()
@@ -98,7 +98,7 @@ class execute:
                 self.badges.output_usage(command['Usage'])
             else:
                 args = self.formatter.format_arguments(arguments)
-                if args:
+                if args[0]:
                     command['Args'] = args
                 try:
                     command['Run']()
