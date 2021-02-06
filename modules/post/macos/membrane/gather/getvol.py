cea613b299b75b2a8fa3c179b10960d83f222889
Ivan Nikolsky <enty8080@gmail.com>
2021-02-06 14:04:44 +0300

    Update getvol.py

--- modules/post/macos/membrane/gather/getvol.py
+++ modules/post/macos/membrane/gather/getvol.py
@@ -42,6 +42,9 @@ class ZetaSploitModule:
                 'enty8080'
             ],
             'Description': "Get device volume level.",
+            'Dependencies': [
+                ''
+            ],
             'Comments': [
                 ''
             ],
